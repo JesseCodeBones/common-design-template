2cc6510c217929cfa449e29ed7dbbbe6ca6f33f3
JesseCodeBones <56120624+JesseCodeBones@users.noreply.github.com>
2022-02-28 14:48:58 +0800

    NIO

---
+++ nio/nio-file-channel/src/main/kotlin/FileNIOTestMain.kt
@@ -0,0 +1,41 @@
+import java.io.FileOutputStream
+import java.nio.channels.FileChannel
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import java.time.LocalDateTime
+import kotlin.concurrent.thread
+
+
+class FileNIOTestMain {
+
+    companion object {
+
+    }
+
+    fun writeAndRead(){
+        val userDir = System.getProperty("user.dir")
+        val strToWrite = "hello world"
+        Files.write(Paths.get("$userDir/build/test.text"),
+            strToWrite.byteInputStream(Charsets.UTF_8).readAllBytes(),
+        StandardOpenOption.APPEND)
+
+        repeat(300){
+            thread {
+                val result = Files.readString(Paths.get("$userDir/build/test.text"))
+                if (it == 299) {
+                    println(LocalDateTime.now())
+                }
+                println(it)
+            }
+        }
+
+    }
+}
+
+fun main() {
+    println(LocalDateTime.now())
+    FileNIOTestMain().writeAndRead()
+
+}
